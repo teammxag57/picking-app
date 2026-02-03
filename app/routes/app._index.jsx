187d739d8699e279ec744e52f602aed7de5c2ffd
Caldeira Caldeira&Caldeira Caldeira <geral@caldeiraecaldeira.pt>
2026-02-03 21:09:29 +0000

    Fix json import from react-router

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,4 +1,4 @@
-import { json } from "react-router";
+import { json } from "@shopify/remix-oxygen";
 import { useLoaderData } from "react-router";
 import { Page, Card, Text, BlockStack } from "@shopify/polaris";
 import { authenticate } from "~/shopify.server";
